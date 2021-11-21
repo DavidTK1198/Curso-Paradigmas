bbd74bcb821d11740678f1284353f3d5d5b47898
DavidTK1198 <ddanielb09@gmail.com>
2021-11-20 19:13:01 -0600

    to fix

--- SegundoProyecto.pl
+++ SegundoProyecto.pl
@@ -13,11 +13,31 @@
 % 
 % ===============================================
 %
+%Predicados de apoyo
 pow(_, 0, 1):-!.
 pow(X, N, Total) :- N0 is N - 1, 
-pow(X, N0, Acum), Total is X * Acum.%No utilizada
+pow(X, N0, Acum), Total is X * Acum.
 
+producto([],_,[_]).
+
+producto([0|Ls0],A,[_]):-
+ A0 is A+1,
+producto(Ls0,A0,[_]).
+
+producto([Z | Ls0],A,[X]):-
+N is A*Z,
+A0 is A+1.
+
+insertar(X,[],[X]).
+insertar(X,[A|Ar],[X,A|Ar]) :- =<(X,A).
+insertar(X,[A|Ar],[A|Br]) :-
+	>(X,A), insertar(X,Ar,Br).
+
+%-----------------------------------------------------------------
 
 evaluar(_, [], 0).
 evaluar(X, [Z | Ls0], D) :- 
 evaluar(X, Ls0, R0), D is Z + (X * R0).
+
+derivar([X],D):- producto(X,0,D).
+
