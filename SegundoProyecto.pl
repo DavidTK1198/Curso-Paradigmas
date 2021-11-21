13fe18c266d57ed2bf16e53afac93092d5c6670f
DavidTK1198 <ddanielb09@gmail.com>
2021-11-21 14:03:35 -0600

    Función Derivar

--- SegundoProyecto.pl
+++ SegundoProyecto.pl
@@ -15,29 +15,25 @@
 %
 %Predicados de apoyo
 pow(_, 0, 1):-!.
-pow(X, N, Total) :- N0 is N - 1, 
+pow(X, N, Total) :- N0 is N - 1, %No utilizada
 pow(X, N0, Acum), Total is X * Acum.
-
-producto([],_,[_]).
-
-producto([0|Ls0],A,[_]):-
- A0 is A+1,
-producto(Ls0,A0,[_]).
-
-producto([Z | Ls0],A,[X]):-
-N is A*Z,
-A0 is A+1.
-
-insertar(X,[],[X]).
-insertar(X,[A|Ar],[X,A|Ar]) :- =<(X,A).
-insertar(X,[A|Ar],[A|Br]) :-
-	>(X,A), insertar(X,Ar,Br).
-
 %-----------------------------------------------------------------
 
 evaluar(_, [], 0).
 evaluar(X, [Z | Ls0], D) :- 
 evaluar(X, Ls0, R0), D is Z + (X * R0).
 
-derivar([X],D):- producto(X,0,D).
+derivar(_, 0, []).
+derivar([X0 | Ls0], N, D) :-
+    N0 is N - 1,
+    derivar(Ls0, N0, D0),
+    X is X0 * N,
+    D = [X | D0].
+
+derivar(Ls0, D) :-
+    reverse(Ls0, Ls),
+    length(Ls, N0),
+    N is N0 - 1,
+    derivar(Ls, N, D0),
+    reverse(D0, D).
 
